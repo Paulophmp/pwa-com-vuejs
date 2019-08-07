f24719c23e9f326fa8a00276800637de66e8e787
Paulo Henrique <paulotux.mendes@gmail.com>
2019-08-07 14:39:42 -0300

    Criando service worker

--- exemplo/service-worker.js
+++ exemplo/service-worker.js
@@ -1,5 +1,18 @@
+const cacheName = 'conhecendo-service-workers';
+const filesToCacahe = [
+  '/',
+  'index.html',
+  'style.css',
+];
+
 self.addEventListener('install', (e) => {
   console.log('[ServiceWorker] Installer');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCacahe);
+    }),
+  );
 });
 
 self.addEventListener('activate', (e) => {
@@ -8,4 +21,10 @@ self.addEventListener('activate', (e) => {
 
 self.addEventListener('fetch', (e) => {
   console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      console.log('[ServiceWorker] Get cache for', e.request.url);
+      return response || fetch(e.request);
+    }),
+  );
 });
